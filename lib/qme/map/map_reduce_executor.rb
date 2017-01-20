48f6a5f73aa3437ffd0f5346a2e27de9d7cbcfc3
jeffreywpearson <jeffrey.pearson@cgm.com>
2017-01-20 10:13:15 -0800

    tried adding the allowDiskUse again
    tried adding the allowDiskUse again

--- lib/qme/map/map_reduce_executor.rb
+++ lib/qme/map/map_reduce_executor.rb
@@ -127,7 +127,8 @@ module QME
           QME::QualityReport::EXCLUSIONS => {"$sum" => "$value.#{QME::QualityReport::EXCLUSIONS}"},
           QME::QualityReport::EXCEPTIONS => {"$sum" => "$value.#{QME::QualityReport::EXCEPTIONS}"},
           QME::QualityReport::MSRPOPL => {"$sum" => "$value.#{QME::QualityReport::MSRPOPL}"},
-          QME::QualityReport::CONSIDERED => {"$sum" => 1}
+          QME::QualityReport::CONSIDERED => {"$sum" => 1},
+          {'allowDiskUse' => true}
         }}
 
         aggregate = get_db.command(:aggregate => 'patient_cache', :pipeline => pipeline)
